b2463e5729f840a9fe64aad77fefbddcfe967cfe
irene <irenekurien01@gmail.com>
2021-06-13 22:32:39 +0530

    fixed index.html

--- Grocery_Store/urls.py
+++ Grocery_Store/urls.py
@@ -22,5 +22,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
